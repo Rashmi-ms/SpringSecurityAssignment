580691a0cc7a08f8ddd9dcf567d43350e0ef30aa
Rashmi-ms <rashmims56@gmail.com>
2021-05-15 09:32:32 +0530

    password

---
+++ securitypassword/src/main/java/com/spring/password/SecuritypasswordApplication.java
@@ -0,0 +1,13 @@
+package com.spring.password;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritypasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritypasswordApplication.class, args);
+	}
+
+}
